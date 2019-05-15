f3d9c1e77d3ca0c65ee1bad626c36047ef93bbc5
mogardo <mogardolof@gmail.com>
2019-05-15 15:13:21 +0200

    Startpage: added links + styling

--- wp-content/themes/twentyseventeen/page-startpage.php
+++ wp-content/themes/twentyseventeen/page-startpage.php
@@ -9,6 +9,22 @@ get_header();
         I design interwebs, identities &<br>
         digital experiences for:
         </h1>
+        <div class="start-links-wrapper">
+                <div class="start-links-left">
+                <div class="evil">Evil corporations</div>
+                <div class="evil">Dubious motives</div>
+                <a href="..." class="good">Schools</a>
+                <a href="..." class="good">Festivals</a>
+                <div class="evil">Influencer beefs</div>
+            </div>
+            <div class="start-links-right">
+                <a href="..." class="good">Software companies</a>
+                <a href="..." class="good">Myself</a>
+                <div class="evil">Conservatives</div>
+                <div class="evil">"See it as an experience"</div>
+                <a href="..." class="good">Posh plazas</a>
+            </div>
+        </div>
     </div>
 </div>
 

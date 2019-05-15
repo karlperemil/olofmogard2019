26c67b8d93bbba625102a831221a42ee6d6a7588
mogardo <mogardolof@gmail.com>
2019-05-15 12:45:22 +0200

    start-page:removed ej logo

--- wp-content/themes/twentyseventeen/page-startpage.php
+++ wp-content/themes/twentyseventeen/page-startpage.php
@@ -4,14 +4,12 @@ get_header();
 ?>
 <div class="main-title-wrap" id="main-title-wrap">
     <div class="main-title">
-        <figure class="ej-logo main-logo"></figure>
         <h1 class="startpage-title">I’m Emil Jönsson, a creative developer<br>
         based in Stockholm.<br class="break-mobile">After touring<br>
         multiple advertising agencies around<br>
         Scandinavia I started to freelance with<br>
         a focus on <b>3D</b>, <b>VR</b> & <b>Game design</b>.<br>
         </h1>
-        <!-- <a href="/work"><button>See my work</button></a> -->
     </div>
 </div>
 

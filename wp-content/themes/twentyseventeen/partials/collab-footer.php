a9035968634d564c948ec0aa3218baf0c5ab7b54
mogardo <mogardolof@gmail.com>
2019-05-16 13:57:31 +0200

    About: changed color of scroll button

--- wp-content/themes/twentyseventeen/partials/collab-footer.php
+++ wp-content/themes/twentyseventeen/partials/collab-footer.php
@@ -1,6 +1,6 @@
 <section class="collab hideme">
-    <div class="faded"><h1>Business inquiries</h1></div>
-    <p class="collab-mailto"><a href="mailto:hej@olofmogard.com" target="_blank">hej@olofmogard.com</a></p>
+    <!--<div class="faded"><h1>Business inquiries</h1></div>-->
+    <p class="collab-mailto"><a href="mailto:hello@olofmogard.com" target="_blank">hello@olofmogard.com</a></p>
     <p class="thirds hide-desktop"><a href="tel:004672 232 72 88" target="_blank">Phone</a></p>
     <p class="thirds hide-mobile"><a href="tel:004672 232 72 88" target="_blank">Phone</a></p>
     <p class="thirds"><a href="https://www.instagram.com/olofmogard/" target="_blank">Instagram</a></p>

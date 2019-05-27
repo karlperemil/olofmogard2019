a468ec10cfb554aa13acaec910ffe484d4dc6f79
mogardo <mogardolof@gmail.com>
2019-05-27 11:24:58 +0200

    Startpage: added sturekvarteret link

--- wp-content/themes/twentyseventeen/page-startpage.php
+++ wp-content/themes/twentyseventeen/page-startpage.php
@@ -23,7 +23,7 @@ get_header();
                 <a href="http://localhost:8888/blog/" class="good"><span class="stike-under">Myself</span></a>
                 <div class="evil"><span class="strike">Conservatives</span></div>
                 <div class="evil"><span class="strike">"See it as an experience"</span></div>
-                <a href="..." class="good"><span class="stike-under">Posh plazas</span></a>
+                <a href="http://localhost:8888/2016/05/23/sturekvarteret/" class="good"><span class="stike-under">Posh plazas</span></a>
             </div>
         </div>
     </div>

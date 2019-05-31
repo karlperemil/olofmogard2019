158f4973bd5c24cb023c730712c0c29aa4c0a826
mogardo <mogardolof@gmail.com>
2019-05-31 10:40:38 +0200

    Footer: removed phone

--- wp-content/themes/twentyseventeen/partials/collab-footer.php
+++ wp-content/themes/twentyseventeen/partials/collab-footer.php
@@ -1,8 +1,6 @@
 <section class="collab hideme">
     <!--<div class="faded"><h1>Business inquiries</h1></div>-->
     <p class="collab-mailto"><a href="mailto:hello@olofmogard.com" target="_blank">hello@olofmogard.com</a></p>
-    <p class="thirds hide-desktop"><a href="tel:004672 232 72 88" target="_blank">Phone</a></p>
-    <p class="thirds hide-mobile"><a href="tel:004672 232 72 88" target="_blank">Phone</a></p>
     <p class="thirds"><a href="https://www.instagram.com/olofmogard/" target="_blank">Instagram</a></p>
     <p class="thirds"><a href="https://se.linkedin.com/in/olof-mogard-28864aa4" target="_blank">LinkedIn</a></p>
 </section>

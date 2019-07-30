c20c7c172b272c7d5f9c74c6cc004ff5522bd24a
mogardo <mogardolof@gmail.com>
2019-07-30 14:15:51 +0200

    Footer: added dribbble link

--- wp-content/themes/twentyseventeen/partials/collab-footer.php
+++ wp-content/themes/twentyseventeen/partials/collab-footer.php
@@ -3,4 +3,5 @@
     <p class="collab-mailto"><a href="mailto:hello@olofmogard.com" target="_blank">hello@olofmogard.com</a></p>
     <p class="thirds"><a href="https://www.instagram.com/olofmogard/" target="_blank">Instagram</a></p>
     <p class="thirds"><a href="https://se.linkedin.com/in/olof-mogard-28864aa4" target="_blank">LinkedIn</a></p>
+    <p class="thirds"><a href="https://dribbble.com/mogardo" target="_blank">Dribbble</a></p>
 </section>
